2273cf6dce477ddc0d744bc99ac2b8691b09733d
Leon Zhao <lionz2020@gmail.com>
2026-07-08 23:03:09 -0400

    Simplify betting action generation

--- src/betting_abstraction.cc
+++ src/betting_abstraction.cc
@@ -2,61 +2,29 @@
 
 #include <algorithm>
 #include <stdexcept>
+#include <vector>
 
 namespace poker {
 namespace {
 
 constexpr int kActionKeyMultiplier = 1000000;
 
-int Stack(const CompactPublicState& state, int player) {
-  return state.stack[player];
-}
-
-int Contribution(const CompactPublicState& state, int player) {
-  return state.player_contribution[player];
-}
-
-int OutstandingToCall(const CompactPublicState& state, int player) {
-  return std::max(0, Contribution(state, Opponent(player)) -
-                         Contribution(state, player));
-}
-
-int StreetBetSizesSize(const SolverConfig& config, StreetKind street) {
+const std::vector<double>& BetSizesForStreet(const SolverConfig& config,
+                                             StreetKind street) {
   switch (street) {
     case StreetKind::kPreflop:
-      return config.preflop_bet_sizes.size();
+      return config.preflop_bet_sizes.empty() ? config.bet_sizes
+                                              : config.preflop_bet_sizes;
     case StreetKind::kFlop:
-      return config.flop_bet_sizes.size();
+      return config.flop_bet_sizes.empty() ? config.bet_sizes
+                                           : config.flop_bet_sizes;
     case StreetKind::kTurn:
-      return config.turn_bet_sizes.size();
+      return config.turn_bet_sizes.empty() ? config.bet_sizes
+                                           : config.turn_bet_sizes;
     case StreetKind::kRiver:
-      return config.river_bet_sizes.size();
+      return config.river_bet_sizes.empty() ? config.bet_sizes
+                                            : config.river_bet_sizes;
   }
-}
-
-double BetSizeForStreet(const SolverConfig& config,
-                        StreetKind street,
-                        int index) {
-  if (StreetBetSizesSize(config, street) == 0) {
-    return config.bet_sizes[index];
-  }
-
-  switch (street) {
-    case StreetKind::kPreflop:
-      return config.preflop_bet_sizes[index];
-    case StreetKind::kFlop:
-      return config.flop_bet_sizes[index];
-    case StreetKind::kTurn:
-      return config.turn_bet_sizes[index];
-    case StreetKind::kRiver:
-      return config.river_bet_sizes[index];
-  }
-}
-
-int BetSizesSize(const SolverConfig& config, StreetKind street) {
-  const int street_sizes = StreetBetSizesSize(config, street);
-  return street_sizes > 0 ? street_sizes
-                          : static_cast<int>(config.bet_sizes.size());
 }
 
 int ConcreteBetAmount(const CompactPublicState& state, double size) {
@@ -101,40 +69,32 @@ uint8_t BettingAbstraction::actions_for_betting_node(
     int player,
     std::array<GameAction, GameTree::kMaxActionsPerNode>& actions) const {
   uint8_t action_count = 0;
-  const int stack = Stack(state, player);
+  const int stack = state.stack[player];
   if (stack <= 0) {
     return action_count;
   }
 
-  const int to_call = OutstandingToCall(state, player);
+  const int to_call =
+      std::max(0, state.player_contribution[Opponent(player)] -
+                      state.player_contribution[player]);
   if (to_call > 0) {
     AddAction(actions, action_count, ActionKind::kFold, 0);
     AddAction(actions, action_count, ActionKind::kCall,
               std::min(to_call, stack));
-
-    for (int i = 0; i < BetSizesSize(config_, state.street); ++i) {
-      const int raise_amount =
-          to_call + ConcreteBetAmount(
-                        state, BetSizeForStreet(config_, state.street, i));
-      if (raise_amount < stack) {
-        AddActionIfMissing(actions, action_count, ActionKind::kRaise,
-                           raise_amount);
-      }
-    }
-    if (stack > to_call) {
-      AddAction(actions, action_count, ActionKind::kAllIn, stack);
-    }
   } else {
     AddAction(actions, action_count, ActionKind::kCheck, 0);
+  }
 
-    for (int i = 0; i < BetSizesSize(config_, state.street); ++i) {
-      const int bet_amount =
-          ConcreteBetAmount(state, BetSizeForStreet(config_, state.street, i));
-      if (bet_amount < stack) {
-        AddActionIfMissing(actions, action_count, ActionKind::kBet,
-                           bet_amount);
-      }
+  const ActionKind sized_action =
+      to_call > 0 ? ActionKind::kRaise : ActionKind::kBet;
+  for (double bet_size : BetSizesForStreet(config_, state.street)) {
+    const int amount = to_call + ConcreteBetAmount(state, bet_size);
+    if (amount < stack) {
+      AddActionIfMissing(actions, action_count, sized_action, amount);
     }
+  }
+
+  if (to_call == 0 || stack > to_call) {
     AddAction(actions, action_count, ActionKind::kAllIn, stack);
   }
 
